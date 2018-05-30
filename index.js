3c5e42b3fc1ce45552c519498d00c9c8754f7b66
rakeshvora2007 <rakeshvora2007@gmail.com>
2018-05-29 23:04:34 -0400

    Initial app commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req,res) => {
-  res.send({hi:'there'});
-});
+//clientID 33371964461-qj7rq2m6gr34448vb0isk0625rj1d8pi.apps.googleusercontent.com
+//client secret ZaKnBmHtYCEVkG7ghGqpCZy4
+
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
