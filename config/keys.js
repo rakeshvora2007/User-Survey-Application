d5c062dbe8cd07994329ea8cc0f3876a007b28ac
rakeshvora2007 <rakeshvora2007@gmail.com>
2019-02-16 20:38:34 -0500

    mongoose config

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     googleClientID : '33371964461-qj7rq2m6gr34448vb0isk0625rj1d8pi.apps.googleusercontent.com',
-    googleClientSecret : 'ZaKnBmHtYCEVkG7ghGqpCZy4'
+    googleClientSecret : 'ZaKnBmHtYCEVkG7ghGqpCZy4',
+    mongoURI: 'mongodb://admin:password12@ds337985.mlab.com:37985/emaily-dev'
 }
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys.js');
 require('./services/passport.js');
+
+mongoose.connect(keys.mongoURI);
+
 
 const app = express();
 require('./routes/authRoutes.js')(app);
