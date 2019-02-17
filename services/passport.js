c968b4f24834e2b6257e07e0be6ea7d2db05917d
rakeshvora2007 <rakeshvora2007@gmail.com>
2019-02-16 21:13:43 -0500

    models, schema and saving id to db

--- services/passport.js
+++ services/passport.js
@@ -1,12 +1,15 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys.js');
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-    }, (accessToken) => {
-        console.log(accessToken);
+    }, (accessToken, refreshToken, profile, done) => {
+        new User({googleID: profile.id}).save();
     })
 );
